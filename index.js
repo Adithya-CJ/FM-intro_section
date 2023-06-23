efec1a864da1fc325c88b962a382efee2fbc70f2
adithya cj <116732099+Adithya-CJ@users.noreply.github.com>
2023-06-23 14:20:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function dropDown(){
+    $(".features").toggle();
+    $(".down1").toggle();
+    $(".up1").toggle();
+}
+
+function dropDown2(){
+    $(".com").toggle();
+    $(".down2").toggle();
+    $(".up2").toggle();
+}
